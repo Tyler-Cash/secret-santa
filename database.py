e487f68a9872d3db12464310556c283b668ba6ca
Tyler Cash <contact@tylercash.xyz>
2016-10-13 19:40:04 +1100

    Completed create_database method to rapidly create SQLite database

--- database.py
+++ database.py
@@ -4,3 +4,40 @@ import sqlite3
 def get_database(name):
     conn = sqlite3.connect(name)
     return conn
+
+
+def create_tables(db):
+    conn = db
+
+    conn.execute('''CREATE TABLE USER (
+  UserID    INTEGER PRIMARY KEY,
+  Name      TEXT NOT NULL,
+  Password  TEXT NOT NULL);''')
+    conn.commit()
+
+    conn.execute('''CREATE TABLE INTEREST (
+  InterestID  INTEGER PRIMARY KEY,
+  Description TEXT NOT NULL,
+  UserID      INTEGER,
+
+  FOREIGN KEY (UserID) REFERENCES USER (UserID));''')
+    conn.commit()
+
+    conn.execute('''CREATE TABLE FAMILY (
+  FamilyID    INTEGER PRIMARY KEY,
+  Description TEXT NOT NULL,
+  UserID      INTEGER,
+
+  FOREIGN KEY (UserID) REFERENCES USER (UserID));''')
+    conn.commit()
+
+    conn.execute('''CREATE TABLE SANTA (
+  SantaID     INTEGER PRIMARY KEY,
+  Santa       INTEGER,
+  Recipient   INTEGER,
+
+  FOREIGN KEY (Santa) REFERENCES USER (UserID),
+  FOREIGN KEY (Recipient) REFERENCES USER (UserID));''')
+    conn.commit()
+
+    return conn
