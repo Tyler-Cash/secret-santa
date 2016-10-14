7c8d9ddb3283e08d5a511afcce8730d98fdcfd5a
Tyler Cash <contact@tylercash.xyz>
2016-10-14 18:47:36 +1100

    User now notified of no interests

--- static/javascripts/interest.js
+++ static/javascripts/interest.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
-    $.getJSON($SCRIPT_ROOT + '/ajax-get-interests', {
-    }, function (data) {
+    $.getJSON($SCRIPT_ROOT + '/ajax-get-interests', {}, function (data) {
         $("#result").removeClass();
         $("#interest-holder").addClass("card-panel #ff8a80 red accent-1");
-        if (!data.success) {
+        var getInterestsFailed = false;
+        if (data.success) {
+            if (data.outcome.length != 0) {
+                var html = "<span class='card-title'>Your interests</span><ul class='collection'>";
 
-            $("#interest-holder").html("<p>Error, couldn't get interests from server</p>");
-        } else {
-            var html = "<span class='card-title'>Your interests</span><ul class='collection'>";
-
-            for(var i =0; i < data.outcome.length; i++){
-                html += "<li class='collection-item'>" + data.outcome[i][0] + "<a name='interest-delete' id='" + data.outcome[i][1] +"' class='right'>X</a></li>";
+                for (var i = 0; i < data.outcome.length; i++) {
+                    html += "<li class='collection-item'>" + data.outcome[i][0] + "<a name='interest-delete' id='" + data.outcome[i][1] + "' class='right'>X</a></li>";
+                }
+                $("#interest-holder").html(html + "</ul>");
+            } else {
+                getInterestsFailed = true;
             }
-            $("#interest-holder").html(html + "</ul>");
+        } else {
+            getInterestsFailed = true;
+        }
+
+        if(getInterestsFailed){
+            $("#interest-holder").html("<span class='card-title'>Your interests</span><p>You appear to have no interests yet.</p>");
         }
     });
 
