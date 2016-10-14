b566aa944dc03ca22d3d4cab9187c1c01867d55c
Tyler Cash <contact@tylercash.xyz>
2016-10-14 18:43:50 +1100

    Interests updating through the use of JSON

--- static/javascripts/interest.js
+++ static/javascripts/interest.js
@@ -2,12 +2,17 @@ $(document).ready(function () {
     $.getJSON($SCRIPT_ROOT + '/ajax-get-interests', {
     }, function (data) {
         $("#result").removeClass();
+        $("#interest-holder").addClass("card-panel #ff8a80 red accent-1");
         if (!data.success) {
-            $("#interest-holder").addClass("card-panel #ff8a80 red accent-1");
+
             $("#interest-holder").html("<p>Error, couldn't get interests from server</p>");
         } else {
-            $("#result").addClass("card-panel #b9f6ca green accent-1");
-            $("#interest-holder").html(data.outcome);
+            var html = "<span class='card-title'>Your interests</span><ul class='collection'>";
+
+            for(var i =0; i < data.outcome.length; i++){
+                html += "<li class='collection-item'>" + data.outcome[i][0] + "<a name='interest-delete' id='" + data.outcome[i][1] +"' class='right'>X</a></li>";
+            }
+            $("#interest-holder").html(html + "</ul>");
         }
     });
 
