97317a74773d61d2ad30a038b0ac08892d46ec30
Tyler Cash <contact@tylercash.xyz>
2016-10-19 02:57:20 +1100

    corrected bug where only 1 family were getting secret-santa's

--- find_santa.py
+++ find_santa.py
@@ -24,10 +24,21 @@ def removeUser(person_ID, origianl_list):
             list.append(member)
     return list
 
+
 db = database.get_database('database.db')
-family1 = user.get_from_family(db,1)
-family2 = user.get_from_family(db, 2)
+backup1 = user.get_from_family(db, 1)
+backup2 = user.get_from_family(db, 2)
+
+family1 = backup1
+family2 = backup2
 for user in family1:
     rand_person = select_random_user(family2)
-    santa.add_recipient(user[1], rand_person,db)
+    santa.add_recipient(user[1], rand_person, db)
     family2 = removeUser(rand_person, family2)
+
+family1 = backup1
+family2 = backup2
+for user in family2:
+    rand_person = select_random_user(family1)
+    santa.add_recipient(user[1], rand_person, db)
+    family1 = removeUser(rand_person, family1)
